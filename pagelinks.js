67671604f925f2b6f0fd4f177089a27ca8f2ece8
Edwin Rosario <erosario@nyit.edu>
2014-07-02 20:55:09 -0400

    Create pagelinks.js

---
+++ pagelinks.js
@@ -0,0 +1,22 @@
+function enterpage(){
+	
+	switch(sliderNext){
+	case 1:
+		document.location.href="YOUR LINK1 HERE";
+	 	break;
+	case 2:
+		document.location.href="YOUR LINK2 HERE";
+		break;
+	case 3:
+		document.location.href="YOUR LINK3 HERE";
+		break;
+	case 4:
+		document.location.href="YOUR LINK4 HERE";
+		break;
+	case 5: 
+		document.location.href="YOUR LINK5 HERE";
+		break;
+	default:
+		document.location.href="YOUR LINK6 ETC HERE";
+	}
+}
